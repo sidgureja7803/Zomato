d21aad02db6a9e3141eca5085e1c89c6a193cfad
Siddhant <siddhantgureja39@gmail.com>
2024-10-10 11:57:39 +0530

    Final commit

---
+++ src/data/rasturant.js
@@ -0,0 +1,94 @@
+export const rasturants = [
+    {
+        id: 1,
+        name: 'KFC',
+        price: '₹250 for one',
+        rated: '4.2',
+        time: '36 min',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/6/800126/9410759d611db9c62c3acc23c1f27e06_o2_featured_v2.jpg',
+        subImage: 'https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png'
+    },
+    {
+        id: 2,
+        name: 'Marksmen',
+        price: '₹200 for one',
+        rated: '4.4',
+        time: '28 min',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/8/800228/b4975f178b8c9264498a65735f5d4263_o2_featured_v2.jpg',
+        subImage: 'https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png'
+    },
+    {
+        id: 3,
+        name: "McDonald's",
+        price: '₹220 for one',
+        rated: '4.0',
+        time: '25 min',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/0/800260/0094c2b5485c4c2f52c931306442269b_o2_featured_v2.jpg',
+        subImage: 'https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png'
+    },
+    {
+        id: 4,
+        name: "Mahesh's Mansarovar",
+        price: '₹180 for one',
+        rated: '4.1',
+        time: '32 min',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/1/800561/2b381cd36baba26fd2ec309cee7f20f6_o2_featured_v2.jpg',
+        subImage: 'https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png'
+    },
+    {
+        id: 5,
+        name: 'The Belgian Waffle Co.',
+        price: '₹150 for one',
+        rated: '4.3',
+        time: '20 min',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/2/18639812/cfbda35382090ddbf5128a946c235f74_o2_featured_v2.jpg',
+        subImage: 'https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png',
+        note: 'Dine-in not allowed'
+    },
+    {
+        id: 6,
+        name: 'NIC - Natural Ice Creams',
+        price: '₹120 for one',
+        rated: '4.5',
+        time: '15 min',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/5/19316175/022bf8e7feae2e092fa615e50798353b_o2_featured_v2.jpg',
+        subImage: 'https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png',
+        note: 'Dine-in not allowed'
+    },
+    {
+        id: 7,
+        name: 'Alamgir Hotel',
+        price: '₹300 for one',
+        rated: '4.0',
+        time: '35 min',
+        image: 'https://b.zmtcdn.com/data/pictures/3/800623/db2af0909d9d47da97d29378b3d36ebc_o2_featured_v2.jpg',
+        subImage: 'https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png'
+    },
+    {
+        id: 8,
+        name: 'Aadab-e-Dastarkhwan',
+        price: '₹240 for one',
+        rated: '4.2',
+        time: '30 min',
+        image: 'https://b.zmtcdn.com/data/pictures/1/800301/207ed2ffee3ba2300464c26b15ba1b0d_o2_featured_v2.jpg',
+        subImage: 'https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png'
+    },
+    {
+        id: 9,
+        name: 'La Pino\'z Pizza',
+        price: '₹200 for one',
+        rated: '4.1',
+        time: '22 min',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/0/18798800/7c9f7c11939dcfdf4602edaa44329167_o2_featured_v2.jpg',
+        subImage: 'https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png'
+    },
+    {
+        id: 10,
+        name: 'Burger King',
+        price: '₹200 for one',
+        rated: '4.0',
+        time: '27 min',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/3/19165593/9f2f843523d0e8b9ecd9ee9ee32c1c46_o2_featured_v2.jpg',
+        subImage: 'https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png'
+    },
+];
